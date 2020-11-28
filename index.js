0d6e5f14ba3087971a55a1b1da4ee27fe66601ea
PauloHenrique2 <70448970+PauloHenrique2@users.noreply.github.com>
2020-11-28 15:27:39 -0300

    Novos Botões!

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
+window.onload = function() {
+    let botaoOK = document.querySelector("#botao");
+    botaoOK.onclick = mostrar_nome_e_idade;
+}
+
 function mostrar_nome_e_idade () {
     let novadv = document.createElement("div");
     let nomeInput = document.querySelector("#nome");
-    let novadv2 = document.createElement("div");
     let idadeInput = document.querySelector("#idade");
     let novoTexto = document.createTextNode(`Olá ${nomeInput.value}, bem vindo ao meu site, pelo visto você tem ${idadeInput.value} anos.`);
-
+    
+    let dv1 = document.querySelector("#dv");
     novadv.appendChild(novoTexto);
-    novadv2.appendChild(novoTexto);
-    document.body.appendChild(novadv);
-    document.body.appendChild(novadv2);
-
+    dv1.appendChild(novadv);
 }
-
-let botaoOK = document.querySelector("#botao");
-botaoOK.onclick = mostrar_nome_e_idade;
